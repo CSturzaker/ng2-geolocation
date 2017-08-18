555cf2e174365ec66019aa74b74625ad7a567fb6
Craig Sturzaker <craig.sturzaker@arup.com>
2017-08-18 11:23:03 +0100

    added geolocation service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
+import { GeolocationService } from './geolocation.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  // positions$: Observable<Array<any>>;
+  positions: Array<any> = [];
+
+  constructor(private geolocationService: GeolocationService) {
+    geolocationService.getLocation().forEach(position => {
+      // this.positions$ = position;
+      this.positions.push(position);
+      console.log(position);
+    });
+  }
 }
+
